4c7981aa35de487c2a3f33e359037842e579ab98
gittugaurav <gauravsahu160902@gmail.com>
2022-11-18 15:17:30 +0530

    w
    w

---
+++ pay.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">BIT CANTEEN</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Order</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Google Ads</a>
+      </li>
+
+   
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1 class="text-uppercase" >BIT CANTEEN :) </h1> 
+  <p class="text-capitalize"> Thanks for ordering this item just scan this code and pay now and we will give your food in few minutes <span class="text-uppercase">order,pay and enjoy the meal</span>.</p> 
+</div>
+<section class="m-5">
+  <div >
+    <h3 class="text-center"> SCAN NOW </h3>
+  </div>
+  <div class="row mt-8">
+    <div class="col-lg-4 col-5">
+      <img src="images/pay.jpeg" class="img-fluid aboutimg" >
+    </div>
+    <div class="col-lg-6 col-12">
+      <h2 class="display-4"> ENJOY YOUR MEAL </h2>
+      <a href="#" class="btn btn-success">Wanna to order More</a>   
+    </div>
+  </div>
+</section>
+
+
+<footer>
+  <div class=" bg-dark text-center text-capitalize">
+    <h5 class="py-3 text-white">@Bitcanteen</h5>
+  </div>
+</footer>
+
+</body>
+</html>
